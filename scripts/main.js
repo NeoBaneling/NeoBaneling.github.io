c6e3f56448b452af7dcd94d7a79ca5359fc26fb4
Joey Bishop <jbishop47@gatech.edu>
2019-10-11 00:07:32 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 let heading = document.querySelector('h1');
-heading.textContext = 'Hello World!';
+heading.textContent = 'Hello World!';
 let text1 = document.querySelector('p');
-text1.textContext = "Let's see if this replaces anything.";
+text1.textContent = "Let's see if this replaces anything.";
