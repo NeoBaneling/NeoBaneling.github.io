9561d550c573a38b757a6aebb2a8f84f7cf5e589
Joey Bishop <jbishop47@gatech.edu>
2019-10-11 00:16:23 -0400

    Messing Around

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 let heading = document.querySelector('h1');
-heading.textContent = 'Hello World!';
+heading.textContent = 'Programming and Photoplay';
+let subheading = document.querySelector('h2');
+subheading.textContent = 'A Portfolio by a student who can code things and make them look good too';
 let text1 = document.querySelector('p');
 text1.textContent = "Let's see if this replaces anything.";
