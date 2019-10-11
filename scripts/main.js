26f02443b8ccd6a004e959e654f2c6c742a87a16
Joey Bishop <jbishop47@gatech.edu>
2019-10-11 00:40:38 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 let heading = document.querySelector('h1');
 heading.textContent = 'Programming and Photoplay';
 heading.style.background = '#121476';
-heading.style.backgroundSize = '20px';
+heading.style.backgroundSize = 'auto 20px';
 let subheading = document.querySelector('h2');
 subheading.textContent = 'A Portfolio by a student who can code things and make them look good too';
 let text1 = document.querySelector('p');
