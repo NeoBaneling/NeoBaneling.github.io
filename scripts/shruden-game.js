d9171f0f8e71275330a1fa26f46fe85c2c1ccd7a
Joey Bishop <jbishop47@gatech.edu>
2019-11-01 12:47:39 -0400

    Crafty Shruden updates

--- scripts/shruden-game.js
+++ scripts/shruden-game.js
@@ -1,4 +1,5 @@
-Crafty.init(500,350, document.getElementById('game'));
+Crafty.init(640, 480, document.getElementById('game'));
+
 Crafty.e('2D, DOM, Color, Fourway')
     .attr({x: 0, y: 0, w: 100, h: 100})
     .color('#F00')
