db0f471a310c7adbafe27154b9f6a0617b2450cc
Joey Bishop <jbishop47@gatech.edu>
2019-12-11 17:13:48 -0500

    Update shruden-game.js

--- scripts/shruden-game.js
+++ scripts/shruden-game.js
@@ -1,6 +1,6 @@
 Crafty.init(480, 320, document.getElementById('game'));
 
 Crafty.e('2D, DOM, Color, Fourway')
-    .attr({x: 0, y: 0, w: 16, h: 16})
-    .color('#00F')
+    .attr({x: 0, y: 0, w: 32, h: 32})
+    .color('#0FF')
     .fourway(200);
