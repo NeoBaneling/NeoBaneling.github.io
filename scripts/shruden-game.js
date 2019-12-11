1975a8911d398fa3d825a90dc472fabfd384b31c
Joey Bishop <jbishop47@gatech.edu>
2019-12-11 17:45:22 -0500

    Update shruden-game.js

--- scripts/shruden-game.js
+++ scripts/shruden-game.js
@@ -9,15 +9,15 @@ Crafty.e('2D, DOM, Color, Fourway')
         if (e.key == Crafty.keys.W && this.y > 0) {
             this.y = this.y - 1;
         }
-        if (e.key == Crafty.keys.S && this.y < Crafty.viewport.bounds.max.y - this.attr.h) {
+        if (e.key == Crafty.keys.S && this.y < 320 - this.attr.h) {
             this.y = this.y + 1;
         }
         if (e.key == Crafty.keys.A && this.x > 0) {
             this.x = this.x - 1;
         }
-        if (e.key == Crafty.keys.D && this.x < Crafty.viewport.bounds.max.x - this.attr.w) {
+        if (e.key == Crafty.keys.D && this.x < 480 - this.attr.w) {
             this.x = this.x + 1;
         }
 
-        console.log(this.x, this.y, Crafty.viewport.bounds.max.x, Crafty.viewport.bounds.max.y);
+        console.log(this.x, this.y);
     });
