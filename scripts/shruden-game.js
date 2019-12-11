1e951103a3119303be12279ec2c2cd8d7e91dc8a
Joey Bishop <jbishop47@gatech.edu>
2019-12-11 17:25:37 -0500

    Update shruden-game.js

--- scripts/shruden-game.js
+++ scripts/shruden-game.js
@@ -3,4 +3,19 @@ Crafty.init(480, 320, document.getElementById('game'));
 Crafty.e('2D, DOM, Color, Fourway')
     .attr({x: 0, y: 0, w: 32, h: 32})
     .color('#0FF')
-    .fourway(200);
+    .fourway(200)
+    .bind("KeyDown", function(e) {
+
+        if (e.key == Crafty.keys.W && this.y > 0) {
+            this.y = this.y - 1;
+        }
+        if (e.key == Crafty.keys.S && this.y < Crafty.stage.y - this.attr.h) {
+            this.y = this.y + 1;
+        }
+        if (e.key == Crafty.keys.A && this.x > 0) {
+            this.x = this.x - 1;
+        }
+        if (e.key == Crafty.keys.D && this.x < Crafty.stage.x - this.attr.w) {
+            this.x = this.x + 1;
+        }
+    });
