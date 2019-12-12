87a565cff1a4b22d311cd4ede523a546e4c3bb87
Joey Bishop <jbishop47@gatech.edu>
2019-12-11 20:04:46 -0500

    Update shruden-game.js

--- scripts/shruden-game.js
+++ scripts/shruden-game.js
@@ -1,10 +1,10 @@
 Crafty.init(480, 320, document.getElementById('game'));
 
-var blip = Crafty.e('2D, DOM, Color, Bind')
+var blip = Crafty.e('2D, DOM, Color, Fourway, Bind')
     .attr({x: 0, y: 0, w: 32, h: 32})
     .color('#0FF')
-    //.fourway(200)
-    .bind("KeyDown", function(e) {
+    .fourway(200)
+    .bind("Move", function(oldPosition) {
 
         /*
         if (e.key == Crafty.keys.W && this.y > 0) {
@@ -21,21 +21,21 @@ var blip = Crafty.e('2D, DOM, Color, Bind')
         }
         */
 
-        if (e.key == Crafty.keys.W && this.y > 0) {
-            console.log("W");
-            this.y = this.y - 1;
-        }
-        if (e.key == Crafty.keys.S && this.y < 320 - this.h) {
-            console.log("S");
+        if (oldPosition.y <= 0) {
+            console.log("Top Border");
             this.y = this.y + 1;
         }
-        if (e.key == Crafty.keys.A && this.x > 0) {
-            console.log("A");
-            this.x = this.x - 1;
+        if (oldPosition.y >= 320 - this.h) {
+            console.log("Bottom Border");
+            this.y = this.y - 1;
         }
-        if (e.key == Crafty.keys.D && this.x < 480 - this.w) {
-            console.log("D");
+        if (oldPosition.x <= 0) {
+            console.log("Left Border");
             this.x = this.x + 1;
+        }
+        if (oldPosition.x >= 480 - this.w) {
+            console.log("RightBorder");
+            this.x = this.x - 1;
         }
 
         /*
