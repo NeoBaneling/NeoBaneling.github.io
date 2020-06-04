da48cf31824bc7cdc289a3e90c2aa670fa1eb7a7
Joey Bishop <jbishop47@gatech.edu>
2020-06-04 16:23:15 -0400

    Update stuff

--- scripts/honeycomb-generator.js
+++ scripts/honeycomb-generator.js
@@ -45,12 +45,12 @@ animate(
     },
     draw(progress)
     {
-        drawHexagon(progress, Math.random(canvas.width - 120) + 60, Math.random(canvas.height - 180) + 90, 0);
-        drawHexagon(progress, Math.random(canvas.width - 120) + 60, Math.random(canvas.height - 180) + 90, 0);
-        drawHexagon(progress, Math.random(canvas.width - 120) + 60, Math.random(canvas.height - 180) + 90, 0);
-        drawHexagon(progress, Math.random(canvas.width - 120) + 60, Math.random(canvas.height - 180) + 90, 0);
-        drawHexagon(progress, Math.random(canvas.width - 120) + 60, Math.random(canvas.height - 180) + 90, 0);
-        drawHexagon(progress, Math.random(canvas.width - 120) + 60, Math.random(canvas.height - 180) + 90, 0);
+        drawHexagon(progress, 60, 90, 0);
+        drawHexagon(progress, canvas.width - 60, canvas.height - 90, 0);
+        drawHexagon(progress, 140, 300, 0);
+        drawHexagon(progress, canvas.width - 300, canvas.height - 120, 0);
+        drawHexagon(progress, 120, canvas.height - 70, 0);
+        drawHexagon(progress, canvas.width - 120, 240, 0);
     }
 });
 
