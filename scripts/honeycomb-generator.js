9df53a6538663387f48ec97342912452e45df5d4
Joey Bishop <jbishop47@gatech.edu>
2020-06-05 21:44:41 -0400

    Update honeycomb-generator.js

--- scripts/honeycomb-generator.js
+++ scripts/honeycomb-generator.js
@@ -54,6 +54,22 @@ animate(
     }
 });
 
+function startHexagon(x, y, side)
+{
+    animate(
+    {
+        duration: 3000,
+        timing(timeFraction)
+        {
+            return timeFraction;
+        },
+        draw(progress)
+        {
+            drawHexagon(progress, x, y, side);
+        }
+    });
+}
+
 function drawHexagon(progress, x, y, side)
 {
     var length = 28;
@@ -79,93 +95,27 @@ function drawHexagon(progress, x, y, side)
 
     if (Math.floor(Date.now() / 1000) % 10 > 3 && progress >= 1/6 - 0.002 && progress <= 1/6 + 0.002 && withinCanvas(x, y, length))
     {
-        animate(
-        {
-            duration: 6000,
-            timing(timeFraction)
-            {
-                return timeFraction;
-            },
-            draw(progress)
-            {
-                drawHexagon(progress, x, y, (side) % 6);
-            }
-        });
+        startHexagon(x, y, side % 6);
     }
     if (Math.floor(Date.now() / 1000) % 10 > 2 && progress >= 2/6 - 0.002 && progress <= 2/6 + 0.002 && withinCanvas(x, y, length))
     {
-        animate(
-        {
-            duration: 6000,
-            timing(timeFraction)
-            {
-                return timeFraction;
-            },
-            draw(progress)
-            {
-                drawHexagon(progress, x, y, (1 + side) % 6);
-            }
-        });
+        startHexagon(x, y, (1 + side) % 6)
     }
     if (Math.floor(Date.now() / 1000) % 10 > 1 && progress >= 3/6 - 0.002 && progress <= 3/6 + 0.002 && withinCanvas(x, y, length))
     {
-        animate(
-        {
-            duration: 6000,
-            timing(timeFraction)
-            {
-                return timeFraction;
-            },
-            draw(progress)
-            {
-                drawHexagon(progress, x, y, (2 + side) % 6);
-            }
-        });
+        startHexagon(x, y, (2 + side) % 6);
     }
     if (Math.floor(Date.now() / 1000) % 10 > 5 && progress >= 4/6 - 0.002 && progress <= 4/6 + 0.002 && withinCanvas(x, y, length))
     {
-        animate(
-        {
-            duration: 6000,
-            timing(timeFraction)
-            {
-                return timeFraction;
-            },
-            draw(progress)
-            {
-                drawHexagon(progress, x, y, (3 + side) % 6);
-            }
-        });
+        startHexagon(x, y, (3 + side) % 6);
     }
     if (Math.floor(Date.now() / 1000) % 10 > 4 && progress >= 5/6 - 0.002 && progress <= 5/6 + 0.002 && withinCanvas(x, y, length))
     {
-        animate(
-        {
-            duration: 6000,
-            timing(timeFraction)
-            {
-                return timeFraction;
-            },
-            draw(progress)
-            {
-                drawHexagon(progress, x, y, (4 + side) % 6);
-            }
-        });
+        startHexagon(x, y, (4 + side) % 6);
     }
     if (Math.floor(Date.now() / 1000) % 10 > 7 && progress >= 6/6 - 0.002 && progress <= 6/6 + 0.002 && withinCanvas(x, y, length))
     {
-        animate(
-        {
-            duration: 6000,
-            timing(timeFraction)
-            {
-                return timeFraction;
-            },
-            draw(progress)
-            {
-                drawHexagon(progress, x, y, (5 + side) % 6);
-            }
-        });
+        startHexagon(x, y, (5 + side) % 6);
     }
 
     /*
