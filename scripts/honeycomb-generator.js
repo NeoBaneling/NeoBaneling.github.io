8496d4227383c3d9f8130cc60e6d664e3e280f66
Joey Bishop <jbishop47@gatech.edu>
2020-06-05 21:58:08 -0400

    Update honeycomb-generator.js

--- scripts/honeycomb-generator.js
+++ scripts/honeycomb-generator.js
@@ -75,7 +75,7 @@ function drawHexagon(progress, x, y, side)
     var length = 28;
 
     ctx.beginPath();
-    ctx.strokeStyle = "rgba(188,144,64"+(canvas.width/2-x)/canvas.width/2+")";
+    ctx.strokeStyle = "rgba(188,144,64,"+(canvas.width/2-x)/canvas.width/2+")";
     ctx.lineWidth = 0.5;
     ctx.moveTo(x + length * Math.sin(side), y + length * Math.cos(side));
 
