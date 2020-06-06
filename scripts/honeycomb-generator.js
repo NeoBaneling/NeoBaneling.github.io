1fa26e3e4285d97cef6c79254aebceb7ecf95fbc
Joey Bishop <jbishop47@gatech.edu>
2020-06-05 21:50:57 -0400

    Update honeycomb-generator.js

--- scripts/honeycomb-generator.js
+++ scripts/honeycomb-generator.js
@@ -105,15 +105,15 @@ function drawHexagon(progress, x, y, side)
     {
         startHexagon(x, y, (2 + side) % 6);
     }
-    if (Math.floor(Date.now() / 1000) % 10 > 2 && progress >= 4/6 - 0.002 && progress <= 4/6 + 0.002 && withinCanvas(x, y, length))
+    if (Math.floor(Date.now() / 1000) % 10 > 3 && progress >= 4/6 - 0.002 && progress <= 4/6 + 0.002 && withinCanvas(x, y, length))
     {
         startHexagon(x, y, (3 + side) % 6);
     }
-    if (Math.floor(Date.now() / 1000) % 10 > 4 && progress >= 5/6 - 0.002 && progress <= 5/6 + 0.002 && withinCanvas(x, y, length))
+    if (Math.floor(Date.now() / 1000) % 10 > 2 && progress >= 5/6 - 0.002 && progress <= 5/6 + 0.002 && withinCanvas(x, y, length))
     {
         startHexagon(x, y, (4 + side) % 6);
     }
-    if (Math.floor(Date.now() / 1000) % 10 > 1 && progress >= 6/6 - 0.002 && progress <= 6/6 + 0.002 && withinCanvas(x, y, length))
+    if (Math.floor(Date.now() / 1000) % 10 > 7 && progress >= 6/6 - 0.002 && progress <= 6/6 + 0.002 && withinCanvas(x, y, length))
     {
         startHexagon(x, y, (5 + side) % 6);
     }
