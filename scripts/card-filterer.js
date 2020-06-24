7f4435f7afaad4c165595a3063074056e7ae7202
Joey Bishop <jbishop47@gatech.edu>
2020-06-24 13:39:12 -0400

    Update card-filterer.js

--- scripts/card-filterer.js
+++ scripts/card-filterer.js
@@ -4,39 +4,47 @@
  **/
 
 filterSelection("all")
-function filterSelection(c) {
+function filterSelection(c)
+{
     var x, i;
     x = document.getElementsByClassName("canvas");
     console.log(x);
     if (c == "all") c = "";
     // Add the "show" class (display:block) to the filtered elements,
     // and remove the "show" class from the elements that are not selected
-    for (i = 0; i < x.length; i++) {
+    for (i = 0; i < x.length; i++)
+    {
         w3RemoveClass(x[i], "show");
         if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
     }
 }
 
 // Show filtered elements
-function w3AddClass(element, name) {
+function w3AddClass(element, name)
+{
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {
-        element.className += " " + arr2[i];
+    for (i = 0; i < arr2.length; i++)
+    {
+        if (arr1.indexOf(arr2[i]) == -1)
+        {
+            element.className += " " + arr2[i];
         }
     }
     console.log(element);
 }
 
 // Hide elements that are not selected
-function w3RemoveClass(element, name) {
+function w3RemoveClass(element, name)
+{
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
+    for (i = 0; i < arr2.length; i++)
+    {
+        while (arr1.indexOf(arr2[i]) > -1)
+        {
             arr1.splice(arr1.indexOf(arr2[i]), 1);
         }
       }
@@ -47,8 +55,10 @@ function w3RemoveClass(element, name) {
 // Add active class to the current control button (highlight it)
 var btnContainer = document.getElementById("buttonContainer");
 var btns = btnContainer.getElementsByClassName("button");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
+for (var i = 0; i < btns.length; i++)
+{
+    btns[i].addEventListener("click", function()
+    {
         var current = document.getElementsByClassName("active");
         current[0].className = current[0].className.replace(" active", "");
         this.className += " active";
