3c52a3b82abf970bb990e939ede4d3c66ef72eb1
NeoBaneling <josephbishop1499@yahoo.com>
2025-08-05 21:44:04 -0400

    feat: adds mobile layout

--- src/components/Navigation/index.ts
+++ src/components/Navigation/index.ts
@@ -1,6 +1,7 @@
 import NavBar from './NavBar.astro';
 import NavLink from './NavLink.astro';
+import NavBarHandler from './NavBarHandler.astro';
 import NavBubbleHandler from './NavBubbleHandler.astro';
 
 export default NavBar;
-export { NavLink, NavBubbleHandler };
+export { NavLink, NavBarHandler, NavBubbleHandler };
