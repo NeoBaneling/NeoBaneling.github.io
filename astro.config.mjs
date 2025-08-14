6bb1ea79103a11a3def7729c21303ea41914c93a
NeoBaneling <josephbishop1499@yahoo.com>
2025-08-13 20:47:33 -0400

    feat: adds images and lottie file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
+import lottie from 'astro-integration-lottie';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
+	integrations: [react(), lottie()],
 
 	vite: {
 		plugins: [tailwindcss()]
